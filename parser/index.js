06163108462648dde445272241b6d86c12c60450
Oleh Kravchuk <kolegm.real@gmail.com>
2014-08-14 00:44:36 +0300

    Changed parser

--- parser/index.js
+++ parser/index.js
@@ -56,8 +56,116 @@ function convert (external) {
     if (external.display_name) {
       internal.formatted = external.display_name;
     }
+
+    if (external.address) {
+      if (external.address.country) {
+        internal.country = external.address.country;
+      }
+      if (external.address.country_code) {
+        internal.countryIso = external.address.country_code;
+      }
+      if (external.address.postcode) {
+        internal.zipcode = external.address.postcode;
+      }
+      if (external.address.state) {
+        internal.state = external.address.state;
+      }
+      if (external.address.county) {
+        internal.area = external.address.county;
+      } else if (external.state_district) {
+        internal.area = external.address.state_district;
+      }
+
+      if (external.address.city) {
+        internal.city = external.address.city;
+      }
+      else if (external.address.town) {
+        internal.city = external.address.town;
+      }
+      else if (external.address.village) {
+        internal.city = external.address.village;
+      }
+      else if (external.address.hamlet) {
+        internal.city = external.address.hamlet;
+      }
+
+      if (external.address.city_district) {
+        internal.subcity = external.address.city_district;
+      }
+
+      if (external.address.suburb) {
+        internal.subcity1 = external.address.suburb;
+      }
+
+      if (external.address.locality) {
+        internal.subcity2 = external.address.locality;
+      }
+
+      if (external.address.neighbourhood) {
+        internal.neighborhood = external.address.neighbourhood;
+      }
+
+      if (external.address.road) {
+        internal.streetName = external.address.road;
+      }
+
+      if (external.address.house_number) {
+        internal.streetNumber = external.address.house_number;
+      }
+
+      if (external.address.address29) {
+        internal.streetAddress = external.address.address29;
+      }
+
+      if (external.address.bus_station) {
+        internal.establishment = external.address.bus_station;
+      } else if (external.address.bus_stop) {
+        internal.establishment = external.address.bus_stop;
+      } else if (external.address.marina) {
+        internal.establishment = external.address.marina;
+      } else if (external.address.ferry_terminal) {
+        internal.establishment = external.address.ferry_terminal;
+      } else if (external.address.cafe) {
+        internal.establishment = external.address.cafe;
+      } else if (external.address.restaurant) {
+        internal.establishment = external.address.restaurant;
+      } else if (external.address.parking) {
+        internal.establishment = external.address.parking;
+      } else if (external.address.building) {
+        internal.establishment = external.address.building;
+      } else if (external.address.hairdresser) {
+        internal.establishment = external.address.hairdresser;
+      } else if (external.address.industrial) {
+        internal.establishment = external.address.industrial;
+      }
+    }
+
+    if (external.lat) {
+      internal.latitude = external.lat;
+    }
+    if (external.lon) {
+      internal.longitude = external.lon;
+    }
+
+    if (external.boundingbox) {
+      var viewport = external.boundingbox;
+
+      if (viewport[0]) {
+        internal.viewport.leftTop.latitude = viewport[0];
+      }
+      if (viewport[1]) {
+        internal.viewport.leftTop.longitude = viewport[1];
+      }
+      if (viewport[2]) {
+        internal.viewport.rigthBottom.latitude = viewport[2];
+      }
+      if (viewport[3]) {
+        internal.viewport.rigthBottom.longitude = viewport[3];
+      }
+    }
+
   }
-  
+
   return internal;
 }
 
--- test.js
+++ test.js
@@ -1,12 +1,10 @@
-/**
- * @todo use mocha test
- */
+var util = require('util');
 var communicator = require('./');
 
 const ADDRESS = process.argv[2] || 'Norway, Alesund';
 const LATITUDE = '62.471872';
 const LONGITUDE = '6.149655';
-const LANGUAGE = 'uk';
+const LANGUAGE = 'en';
 
 var options = {
   "accept-language": LANGUAGE
@@ -14,9 +12,9 @@ var options = {
 
 function callback (error, result) {
   if (error) console.log(error);
-  else console.log(result);
+  else console.log(util.inspect(result, {depth: 5}));
 }
 
 communicator.geocode(ADDRESS, callback, options);
 
-communicator.reverseGeocode(LATITUDE, LONGITUDE, callback, options);
+//communicator.reverseGeocode(LATITUDE, LONGITUDE, callback, options);
