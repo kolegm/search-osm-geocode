782aff851bcac49dc849e2695feabdff6a2ebf11
kolegm <kolegm.real@gmail.com>
2014-08-13 18:18:04 +0300

    Changed parser

--- parser/index.js
+++ parser/index.js
@@ -1,16 +1,10 @@
 /**
- * [Doc Google API](http://wiki.openstreetmap.org/wiki/Nominatim)
- *
+ * [Doc Open Street (OSM) Map service](http://wiki.openstreetmap.org/wiki/Nominatim)
  */
 
-//var util = require('util');
 var _ = require('underscore');
 
-function Parser() {}
-
-module.exports.parseData = function (data) {
-  return data;
-};
+var model = require('./model.json');
 
 module.exports.parseError = function (error) {
   if (error && _.isObject(error)) {
@@ -22,3 +16,51 @@ module.exports.parseError = function (error) {
   }
   return error;
 };
+
+/**
+ * Results
+ * When the geocoder returns results, it places them within a (JSON) results array.
+ * Even if the geocoder returns no results (such as if the address doesn't exist) it still returns an empty results array.
+ */
+module.exports.parseData = function (data) {
+  var result = [];
+
+  if (_.isObject(data) && !_.isEmpty(data)) {
+    result = parse(data);
+  }
+
+  return result;
+};
+
+/**
+ * Convert external data format to internal format
+ */
+function parse (externalHolder) {
+  var internalHolder = [];
+  var internal;
+
+  if (_.isArray(externalHolder)) {
+    _.each(externalHolder, function (external) {
+      internal = convert(external);
+      internalHolder.push(internal);
+    });
+  }
+  return internalHolder;
+}
+
+function convert (external) {
+  var internal = create();
+
+  if (!_.isEmpty(external)) {
+
+    if (external.display_name) {
+      internal.formatted = external.display_name;
+    }
+  }
+  
+  return internal;
+}
+
+function create() {
+  return _.extend({}, model);
+}
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  */
 var communicator = require('./');
 
-const ADDRESS = 'Norway, Alesund';
+const ADDRESS = process.argv[2] || 'Norway, Alesund';
 const LATITUDE = '62.471872';
 const LONGITUDE = '6.149655';
 const LANGUAGE = 'uk';
